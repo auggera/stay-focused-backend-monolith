72868f6bc077a4f8a211b3d79b7288e2fd1f2985
Augustin Gerasym <augustingerasym@gmail.com>
2025-03-03 11:59:56 +0100

    Initial commit.

---
+++ src/main/java/one/stayfocused/backend/StayFocusedApplication.java
@@ -0,0 +1,13 @@
+package one.stayfocused.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayFocusedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayFocusedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/stayfocused/backend/StayFocusedApplicationTests.java
@@ -0,0 +1,13 @@
+package one.stayfocused.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayFocusedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
