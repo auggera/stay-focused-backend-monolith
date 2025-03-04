bc7e709a7ebd3bcd10d825a87f8e50bdcf17f3f7
Augustin Gerasym <augustingerasym@gmail.com>
2025-03-04 17:40:23 +0100

    Added @Data annotation to OAuthRegisterRequestDto.

--- src/main/java/one/stayfocused/backend/dto/OAuthRegisterRequestDto.java
+++ src/main/java/one/stayfocused/backend/dto/OAuthRegisterRequestDto.java
@@ -1,8 +1,10 @@
 package one.stayfocused.backend.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 import one.stayfocused.backend.validation.ValidName;
 
+@Data
 public class OAuthRegisterRequestDto {
 
     private String email;
