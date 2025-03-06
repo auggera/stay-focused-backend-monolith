84401d90acd31a11623b3a869278f81f0b2dc066
Augustin Gerasym <augustingerasym@gmail.com>
2025-03-06 20:50:05 +0100

    Defined email as required for OAuth registration.

--- src/main/java/one/stayfocused/backend/dto/OAuthRegisterRequestDto.java
+++ src/main/java/one/stayfocused/backend/dto/OAuthRegisterRequestDto.java
@@ -2,11 +2,14 @@ package one.stayfocused.backend.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import one.stayfocused.backend.validation.ValidEmail;
 import one.stayfocused.backend.validation.ValidName;
 
 @Data
 public class OAuthRegisterRequestDto {
 
+    @NotBlank
+    @ValidEmail
     private String email;
 
     @NotBlank
