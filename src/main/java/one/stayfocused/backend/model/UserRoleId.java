b6127f34b3e7970e60bfa6c10999f6caff1104e9
Augustin Gerasym <augustingerasym@gmail.com>
2025-03-07 12:49:32 +0100

    Modified the UserRoleId and UserRole classes to provide immutability for the corresponding fields.

--- src/main/java/one/stayfocused/backend/model/UserRoleId.java
+++ src/main/java/one/stayfocused/backend/model/UserRoleId.java
@@ -2,16 +2,24 @@ package one.stayfocused.backend.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.io.Serializable;
 
+@Getter
+@AllArgsConstructor
 @Embeddable
-@Data
 public class UserRoleId implements Serializable {
     @Column(name = "user_id")
-    private Long userId;
+    private final Long userId;
 
     @Column(name = "role_id")
-    private Long roleId;
+    private final Long roleId;
+
+    protected UserRoleId() {
+        this.userId = null;
+        this.roleId = null;
+    }
 }
+
