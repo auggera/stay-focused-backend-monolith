290bdd82ef5e65fb219ec142fdf1c98123a817a0
Augustin Gerasym <augustingerasym@gmail.com>
2025-03-07 15:07:49 +0100

    Created method existsByUserAndRole in UserRoleRepository.

--- src/main/java/one/stayfocused/backend/repository/UserRoleRepository.java
+++ src/main/java/one/stayfocused/backend/repository/UserRoleRepository.java
@@ -1,7 +1,10 @@
 package one.stayfocused.backend.repository;
 
+import one.stayfocused.backend.model.Role;
+import one.stayfocused.backend.model.User;
 import one.stayfocused.backend.model.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
+    boolean existsByUserAndRole(User user, Role role);
 }
