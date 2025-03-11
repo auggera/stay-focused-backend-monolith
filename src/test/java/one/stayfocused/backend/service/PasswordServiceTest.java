033b25819b42f5efe4eb7ca02b26460116119c56
Augustin Gerasym <augustingerasym@gmail.com>
2025-03-11 11:27:47 +0100

    Moved the user repository save() call out of the updatePassword() method. Implemented unit tests for method requestChangePasswordWithOtp().

--- src/test/java/one/stayfocused/backend/service/PasswordServiceTest.java
+++ src/test/java/one/stayfocused/backend/service/PasswordServiceTest.java
@@ -1,11 +1,9 @@
 package one.stayfocused.backend.service;
 
+import one.stayfocused.backend.dto.PasswordChangeWithOtpRequestDto;
 import one.stayfocused.backend.dto.PasswordUpdateRequestDto;
 import one.stayfocused.backend.dto.PasswordVerificationRequestDto;
-import one.stayfocused.backend.exception.IncorrectCurrentPasswordException;
-import one.stayfocused.backend.exception.SamePasswordException;
-import one.stayfocused.backend.exception.TokenNotFoundException;
-import one.stayfocused.backend.exception.UserNotFoundException;
+import one.stayfocused.backend.exception.*;
 import one.stayfocused.backend.model.User;
 import one.stayfocused.backend.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -37,14 +35,18 @@ class PasswordServiceTest {
 
     private static final Long USER_ID = 1L;
     private static final String TOKEN_PASSWORD_CHANGE_KEY = "token:password-change:";
+    private static final String OTP_PASSWORD_CHANGE_KEY = "otp:password-change:";
+    private static final String OTP_PASSWORD_RESET_KEY = "otp:password-reset:";
     private static final String VALID_PASSWORD = "validPassword#123";
     private static final String INVALID_PASSWORD = "invalidPassword";
     private static final String NEW_VALID_PASSWORD = "newValidPassword#123";
     private static final String HASHED_PASSWORD = "#g4$Hf5dsd$dsd!ds";
     private static final String NEW_HASHED_PASSWORD = "$jd5fsJHj#ifdfs454";
+    private static final String OTP = "123456";
     private static final PasswordVerificationRequestDto VALID_PASSWORD_VERIFICATION_REQUEST_DTO = new PasswordVerificationRequestDto(VALID_PASSWORD);
     private static final PasswordVerificationRequestDto INVALID_PASSWORD_VERIFICATION_REQUEST_DTO = new PasswordVerificationRequestDto(INVALID_PASSWORD);
-    private static final PasswordUpdateRequestDto VALID_PASSWORD_UPDATE_REQUEST_DTO = new PasswordUpdateRequestDto(NEW_VALID_PASSWORD);
+    private static final PasswordUpdateRequestDto PASSWORD_UPDATE_REQUEST_DTO = new PasswordUpdateRequestDto(NEW_VALID_PASSWORD);
+    private static final PasswordChangeWithOtpRequestDto VALID_PASSWORD_CHANGE_WITH_OTP_REQUEST_DTO = new PasswordChangeWithOtpRequestDto(OTP, NEW_VALID_PASSWORD);
 
     private User user;
 
@@ -54,6 +56,7 @@ class PasswordServiceTest {
 
         user = new User();
         user.setId(USER_ID);
+        user.setEmail("john@doe.com");
         user.setPassword(HASHED_PASSWORD);
 
         lenient().doReturn(valueOperations)
@@ -117,11 +120,12 @@ class PasswordServiceTest {
         doReturn(NEW_HASHED_PASSWORD)
                 .when(passwordEncoder).encode(NEW_VALID_PASSWORD);
 
-        passwordService.changePasswordAfterVerification(USER_ID, VALID_PASSWORD_UPDATE_REQUEST_DTO);
+        passwordService.changePasswordAfterVerification(USER_ID, PASSWORD_UPDATE_REQUEST_DTO);
 
         verify(redisTemplate).opsForValue();
         verify(valueOperations).get(startsWith(TOKEN_PASSWORD_CHANGE_KEY));
         verify(redisTemplate).delete(startsWith(TOKEN_PASSWORD_CHANGE_KEY));
+        verify(userRepository).save(user);
     }
 
     @Test
@@ -130,7 +134,7 @@ class PasswordServiceTest {
             .when(userRepository).findById(USER_ID);
 
         assertThrows(UserNotFoundException.class,
-                ()  -> passwordService.changePasswordAfterVerification(USER_ID, VALID_PASSWORD_UPDATE_REQUEST_DTO)
+                ()  -> passwordService.changePasswordAfterVerification(USER_ID, PASSWORD_UPDATE_REQUEST_DTO)
         );
     }
 
@@ -150,7 +154,7 @@ class PasswordServiceTest {
                 .when(valueOperations).get(startsWith(TOKEN_PASSWORD_CHANGE_KEY));
 
         assertThrows(TokenNotFoundException.class,
-                () -> passwordService.changePasswordAfterVerification(USER_ID, VALID_PASSWORD_UPDATE_REQUEST_DTO)
+                () -> passwordService.changePasswordAfterVerification(USER_ID, PASSWORD_UPDATE_REQUEST_DTO)
         );
     }
 
@@ -166,7 +170,59 @@ class PasswordServiceTest {
                 .when(passwordEncoder).matches(NEW_VALID_PASSWORD, user.getPassword());
 
         assertThrows(SamePasswordException.class,
-                () -> passwordService.changePasswordAfterVerification(USER_ID, VALID_PASSWORD_UPDATE_REQUEST_DTO)
+                () -> passwordService.changePasswordAfterVerification(USER_ID, PASSWORD_UPDATE_REQUEST_DTO)
         );
+    }
+
+    @Test
+    void testShouldSendOtp_whenRequestChangePasswordWithOtp() {
+        final String otpType = "password-change";
+
+        doReturn(Optional.of(user))
+                .when(userRepository).findById(USER_ID);
+
+        doReturn(OTP)
+                .when(otpService).generateOtp(otpType, user.getEmail());
+
+        doNothing()
+                .when(emailService).sendOtp(user.getEmail(), OTP);
+
+        passwordService.requestChangePasswordWithOtp(USER_ID);
+
+        verify(otpService).generateOtp(otpType, user.getEmail());
+        verify(emailService).sendOtp(user.getEmail(), OTP);
+    }
+
+    @Test
+    void testShouldThrowUserNotFoundException_whenRequestChangePasswordWithOtp_andUserDoesNotExists() {
+        assertThrows(UserNotFoundException.class,
+                () -> passwordService.requestChangePasswordWithOtp(USER_ID)
+        );
+    }
+
+    @Test
+    void testShouldChangePasswordWithOtp_whenOtpIsValid() {
+        final String otpType = "password-change";
+
+        doReturn(Optional.of(user))
+                .when(userRepository).findById(USER_ID);
+
+        doNothing()
+                .when(otpService).validateOtp(otpType, user.getEmail(), OTP);
+
+        doReturn(false)
+                .when(passwordEncoder).matches(NEW_VALID_PASSWORD, user.getPassword());
+
+        System.out.println(user.getPassword());
+
+        doReturn(NEW_HASHED_PASSWORD)
+                .when(passwordEncoder).encode(NEW_VALID_PASSWORD);
+
+        passwordService.changePasswordWithOtp(USER_ID, VALID_PASSWORD_CHANGE_WITH_OTP_REQUEST_DTO);
+
+        verify(otpService).validateOtp(otpType, user.getEmail(), OTP);
+        verify(passwordEncoder).matches(NEW_VALID_PASSWORD, HASHED_PASSWORD);
+        verify(passwordEncoder).encode(NEW_VALID_PASSWORD);
+        verify(userRepository).save(user);
     }
 }
