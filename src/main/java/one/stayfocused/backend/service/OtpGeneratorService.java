84561bd5a29247d08aadfb4727e34d3de185ea5f
Augustin Gerasym <augustingerasym@gmail.com>
2025-03-12 13:28:42 +0100

    Implemented OtpGeneratorService.

---
+++ src/main/java/one/stayfocused/backend/service/OtpGeneratorService.java
@@ -0,0 +1,39 @@
+package one.stayfocused.backend.service;
+
+import lombok.RequiredArgsConstructor;
+import one.stayfocused.backend.exception.OtpRequestLimitExceededException;
+import one.stayfocused.backend.repository.OtpRepository;
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.time.Duration;
+
+@RequiredArgsConstructor
+@Service
+public class OtpGeneratorService implements OtpGenerator {
+
+    private final OtpRepository otpRepository;
+    private final OtpRateLimiter otpRateLimitService;
+    private final SecureRandom random = new SecureRandom();
+
+    @Override
+    public String generateOtp(String otpType, String identifier) {
+        if (otpRateLimitService.isRequestLimitExceeded(otpType, identifier)) {
+            throw new OtpRequestLimitExceededException();
+        }
+
+        otpRepository.deleteOtp(otpType, identifier);
+
+        String otp = generate();
+        Duration expiration = otpRepository.getOtpExpiration(otpType);
+
+        otpRepository.saveOtp(otpType, identifier, otp, expiration);
+        otpRateLimitService.incrementOtpRequestCount(otpType, identifier);
+
+        return otp;
+    }
+
+    private String generate() {
+        return String.format("%06d", random.nextInt(1_000_000));
+    }
+}
