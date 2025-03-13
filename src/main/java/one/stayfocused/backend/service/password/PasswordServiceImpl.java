a9240437c5fca74cabaa11cd645abec324c160b8
Augustin Gerasym <augustingerasym@gmail.com>
2025-03-13 12:04:00 +0100

    Separated the logic of changing the password using a token and resetting the password using OTP.

--- src/main/java/one/stayfocused/backend/service/password/PasswordServiceImpl.java
+++ src/main/java/one/stayfocused/backend/service/password/PasswordServiceImpl.java
@@ -2,14 +2,12 @@ package one.stayfocused.backend.service.password;
 
 import lombok.RequiredArgsConstructor;
 import one.stayfocused.backend.dto.*;
-import one.stayfocused.backend.exception.IncorrectCurrentPasswordException;
 import one.stayfocused.backend.exception.SamePasswordException;
 import one.stayfocused.backend.exception.TokenNotFoundException;
 import one.stayfocused.backend.exception.UserNotFoundException;
 import one.stayfocused.backend.model.User;
+import one.stayfocused.backend.repository.EphemeralTokenRepository;
 import one.stayfocused.backend.repository.UserRepository;
-import one.stayfocused.backend.service.EmailService;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,32 +20,28 @@ import static java.util.Objects.requireNonNull;
 @RequiredArgsConstructor
 @Service
 public class PasswordServiceImpl implements PasswordService {
-    private static final String TOKEN_PREFIX = "token:";
-    private static final String OTP_TYPE_PASSWORD_RESET = "password-reset";
-    private static final String OTP_TYPE_PASSWORD_CHANGE = "password-change";
+
     private static final String REQUEST_NULL_ERROR_MESSAGE = "Request must not be null";
+    private static final String TOKEN_TYPE_PASSWORD_CHANGE = "password-change";
     private static final Duration TOKEN_EXPIRATION = Duration.ofMinutes(5);
 
     private final UserRepository userRepository;
-    private final OtpService otpService;
-    private final EmailService emailService;
+    private final EphemeralTokenRepository tokenRepository;
     private final PasswordEncoder passwordEncoder;
-    private final RedisTemplate<String, String> redisTemplate;
 
     @Override
     public void verifyCurrentPassword(Long userId, PasswordVerificationRequestDto request) {
         requireNonNull(request, REQUEST_NULL_ERROR_MESSAGE);
         User user = getUserByIdInternal(userId);
 
-        if (!passwordEncoder.matches(request.currentPassword(), user.getPassword())) {
-            throw new IncorrectCurrentPasswordException();
+        if (!isSamePasswordAsCurrent(request.currentPassword(), user.getPassword())) {
+            throw new SamePasswordException();
         }
 
-        String tokenKey = tokenKeyBuilder(userId, OTP_TYPE_PASSWORD_CHANGE);
-        deleteToken(tokenKey);
+        tokenRepository.deleteToken(TOKEN_TYPE_PASSWORD_CHANGE, user.getEmail());
 
         String token = UUID.randomUUID().toString();
-        redisTemplate.opsForValue().set(tokenKey, token, TOKEN_EXPIRATION);
+        tokenRepository.saveToken(TOKEN_TYPE_PASSWORD_CHANGE, user.getEmail(), token, TOKEN_EXPIRATION);
     }
 
     @Override
@@ -56,59 +50,20 @@ public class PasswordServiceImpl implements PasswordService {
         requireNonNull(request, REQUEST_NULL_ERROR_MESSAGE);
         User user =  getUserByIdInternal(userId);
 
-        String tokenKey = tokenKeyBuilder(userId, OTP_TYPE_PASSWORD_CHANGE);
-        String token = retrieveToken(tokenKey);
+        String token = tokenRepository.getToken(TOKEN_TYPE_PASSWORD_CHANGE, user.getEmail());
 
         if (token == null) {
             throw new TokenNotFoundException("Token not found or expired");
         }
 
-        isSamePasswordAsCurrent(request.newPassword(), user.getPassword());
+        if (isSamePasswordAsCurrent(request.newPassword(), user.getPassword())) {
+            throw new SamePasswordException();
+        }
 
         updatePassword(user, request.newPassword());
         userRepository.save(user);
 
-        deleteToken(tokenKey);
-    }
-
-    @Override
-    public void requestChangePasswordWithOtp(Long userId) {
-        User user = getUserByIdInternal(userId);
-        sendOtp(user.getEmail(), OTP_TYPE_PASSWORD_CHANGE);
-    }
-
-    @Override
-    @Transactional
-    public void changePasswordWithOtp(Long userId, PasswordChangeWithOtpRequestDto request) {
-        requireNonNull(request, REQUEST_NULL_ERROR_MESSAGE);
-        User user = getUserByIdInternal(userId);
-
-        otpService.validateOtp(OTP_TYPE_PASSWORD_CHANGE, user.getEmail(), request.otpCode());
-
-        isSamePasswordAsCurrent(request.newPassword(), user.getPassword());
-        updatePassword(user, request.newPassword());
-        userRepository.save(user);
-    }
-
-    @Override
-    public void requestResetPasswordWithOtp(PasswordResetRequestDto request) {
-        requireNonNull(request, REQUEST_NULL_ERROR_MESSAGE);
-
-        getUserByEmailInternal(request.email());
-        sendOtp(request.email(), OTP_TYPE_PASSWORD_RESET);
-    }
-
-    @Override
-    @Transactional
-    public void resetPasswordWithOtp(PasswordResetWithOtpRequestDto request) {
-        requireNonNull(request, REQUEST_NULL_ERROR_MESSAGE);
-        User user = getUserByEmailInternal(request.email());
-
-        otpService.validateOtp(OTP_TYPE_PASSWORD_RESET, user.getEmail(), request.otpCode());
-
-        isSamePasswordAsCurrent(request.newPassword(), user.getPassword());
-        updatePassword(user, request.newPassword());
-        userRepository.save(user);
+        tokenRepository.deleteToken(TOKEN_TYPE_PASSWORD_CHANGE, user.getEmail());
     }
 
     private User getUserByIdInternal(Long userId) {
@@ -116,36 +71,11 @@ public class PasswordServiceImpl implements PasswordService {
                 .orElseThrow(() -> new UserNotFoundException(userId));
     }
 
-    private User getUserByEmailInternal(String email) {
-        return userRepository.findByEmail(email)
-                .orElseThrow(() -> new UserNotFoundException(email));
-    }
-
-
     private void updatePassword(User user, String newPassword) {
         user.setPassword(passwordEncoder.encode(newPassword));
     }
 
-    private void sendOtp(String email, String otpType) {
-        String otp = otpService.generateOtp(otpType, email);
-        emailService.sendOtp(email, otp);
-    }
-
-    private String tokenKeyBuilder(Long userId, String otpType) {
-        return TOKEN_PREFIX + otpType + ":" + userId;
-    }
-
-    private String retrieveToken(String tokenKey) {
-        return redisTemplate.opsForValue().get(tokenKey);
-    }
-
-    private void deleteToken(String tokenKey) {
-        redisTemplate.delete(tokenKey);
-    }
-
-    private void isSamePasswordAsCurrent(String newPassword, String currentPassword) {
-        if (passwordEncoder.matches(newPassword, currentPassword)) {
-            throw new SamePasswordException();
-        }
+    private boolean isSamePasswordAsCurrent(String newPassword, String currentPassword) {
+        return passwordEncoder.matches(newPassword, currentPassword);
     }
 }
