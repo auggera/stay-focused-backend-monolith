25d505f78b007d85547711ff72f1f0413ef2b6a2
Augustin Gerasym <augustingerasym@gmail.com>
2025-03-29 17:04:49 +0100

    Added default storage type to properties file and AvatarConfig.

--- src/main/java/one/stayfocused/backend/config/AvatarConfig.java
+++ src/main/java/one/stayfocused/backend/config/AvatarConfig.java
@@ -1,6 +1,8 @@
 package one.stayfocused.backend.config;
 
 import lombok.Getter;
+import one.stayfocused.backend.storage.StorageType;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -11,4 +13,7 @@ import java.util.List;
 @ConfigurationProperties(prefix = "avatars")
 public class AvatarConfig {
     private List<String> defaultAvatarUrls;
+
+    @Value("${avatars.storage-type}")
+    private StorageType defaultStorageType;
 }
