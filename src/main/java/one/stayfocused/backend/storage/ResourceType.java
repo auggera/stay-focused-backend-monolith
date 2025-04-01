388efb54c372afa14b328431bbfa36f9a4452b7c
Augustin Gerasym <augustingerasym@gmail.com>
2025-04-01 13:47:49 +0200

    Updated ResourceType adding new field allias and method fromAlias() for confident pasing of resource type from URL or public_ID.

--- src/main/java/one/stayfocused/backend/storage/ResourceType.java
+++ src/main/java/one/stayfocused/backend/storage/ResourceType.java
@@ -1,5 +1,21 @@
 package one.stayfocused.backend.storage;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Getter
 public enum ResourceType {
-    AVATAR
+    AVATAR("avatars");
+
+    private final String alias;
+
+    public static Optional<ResourceType> fromAlias(String value) {
+        return Arrays.stream(values())
+                .filter(type -> type.alias.equalsIgnoreCase(value))
+                .findFirst();
+    }
 }
