ce1b163d0ec647e76ac26077db22227085797bab
Augustin Gerasym <augustingerasym@gmail.com>
2025-04-01 14:49:21 +0200

    Updated StorageType to unify logic with ResourceType.

--- src/main/java/one/stayfocused/backend/storage/StorageType.java
+++ src/main/java/one/stayfocused/backend/storage/StorageType.java
@@ -1,5 +1,21 @@
 package one.stayfocused.backend.storage;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+@Getter
+@RequiredArgsConstructor
 public enum StorageType {
-    LOCAL, CLOUDINARY
+    LOCAL("local"), CLOUDINARY("cloudinary");
+
+    private final String alias;
+
+    public static Optional<StorageType> fromAlias(String value) {
+        return Arrays.stream(values())
+                .filter(type -> type.alias.equals(value))
+                .findFirst();
+    }
 }
