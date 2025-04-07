3a7bd03ee6a4904b0156ed632ad933a063c073e5
Augustin Gerasym <augustingerasym@gmail.com>
2025-04-07 18:59:18 +0200

    Removed ActiveProfile annotation from CloudinaryAvatarStorageServiceIntegrationTest.

--- src/test/java/one/stayfocused/backend/service/avatar/CloudinaryAvatarStorageServiceIntegrationTest.java
+++ src/test/java/one/stayfocused/backend/service/avatar/CloudinaryAvatarStorageServiceIntegrationTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -16,7 +15,6 @@ import java.nio.file.Files;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class CloudinaryAvatarStorageServiceIntegrationTest {
 
     @Autowired
